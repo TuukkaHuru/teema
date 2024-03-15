50ae8ac051397bb63a762760d0cdb43a7b63b1e5
tuukka huru <t1hutu02@students.oamk.fi>
2024-03-15 17:37:56 +0200

    Eka

---
+++ screens/Settings.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import Styles from '../Styles';
+import ThemeSwitchButton from '../components/ThemeSwitchButton';
+import { useTheme } from '../context/UseTheme';
+
+export default function Settings  ()  {
+    const {isDarkMode} = useTheme ()
+  return (
+    <View style={[Styles.container, isDarkMode ? Styles.dark : Styles.light]}>
+      <ThemeSwitchButton/>
+    </View>
+  )
+}
